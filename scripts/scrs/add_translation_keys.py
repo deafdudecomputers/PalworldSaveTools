1a595d91eceb37a39285176c40651df4223e0967
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2026-07-26 13:54:31 -0700

    Revert "feat: base rotation in nudge dialog"
    This reverts commit 4f94846648f522bf527a7808189336c0f54b07a3.

--- scripts/scrs/add_translation_keys.py
+++ scripts/scrs/add_translation_keys.py
@@ -35,7 +35,6 @@ NEW_TRANSLATIONS = {
     'guild.assign.title': 'Guild Assignment',
     'guild.assign.members_label': 'Current Members',
     'guild.assign.members_empty': 'Select a guild to see its members.',
-    'base.nudge.rotate': 'Rotation',
 }
 OLD_KEYS = []
 def _clean_uv_locks():
