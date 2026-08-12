418951f23906659bd1afa054cfeffaba872d102d
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2026-08-12 13:15:29 -0700

    chore: update game data version to 1.0.3

--- src/common.py
+++ src/common.py
@@ -4,7 +4,7 @@ from resource_resolver import get_base_dir, get_src_dir, get_resources_dir, reso
 APP_NAME = 'PalworldSaveTools'
 APP_VERSION = '2.3.5'
 TESTING_VER = '2.3.5'
-GAME_VERSION = '1.0.2'
+GAME_VERSION = '1.0.3'
 def get_base_directory():
     return get_base_dir()
 def get_src_directory():
